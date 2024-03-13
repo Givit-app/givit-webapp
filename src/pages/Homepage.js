b677135b200a5c6fb5d5fab2e12dfbd8ec11b66e
federico_calandra <federico12597@gmial.com>
2024-03-13 13:34:59 +0100

    New Homepage version

--- src/pages/Homepage.js
+++ src/pages/Homepage.js
@@ -8,49 +8,51 @@ import Footer from "../components/Footer";
 import LogoBottom from "../components/LogoBottom";
 
 import homepageImage from '../assets/homepage_image.png';
+import homepageExplanationImage1 from '../assets/homepage_explanation_images/homepage_exp_image1.png'
+import homepageExplanationImage2 from '../assets/homepage_explanation_images/homepage_exp_image2.png'
+import homepageExplanationImage3 from '../assets/homepage_explanation_images/homepage_exp_image3.png'
+import homepageExplanationImage4 from '../assets/homepage_explanation_images/homepage_exp_image4.png'
+import homepageExplanationImage5 from '../assets/homepage_explanation_images/homepage_exp_image5.png'
+import homepageExplanationImage6 from '../assets/homepage_explanation_images/homepage_exp_image6.png'
 
 export default function Homepage() {
     ReactGA.send({ hitType: "pageview", page: "/", title: "Home Page" });
     return (
         <>
-            <Navbar />
+        <Navbar />
             <div className="homepage-content">
                 <div className="homepage-content-stack">
                     {/* eslint-disable-next-line jsx-a11y/img-redundant-alt */}
-                    <img src={homepageImage} alt="Homepage image"/>
                     <p><span>Give it</span> a longer life!</p>
+                    <p id="main_logo">Givit</p>
+                    {/* eslint-disable-next-line jsx-a11y/img-redundant-alt */}
+                    <img src={homepageImage} alt="Homepage image"/>
                 </div>
                 <div className="homepage-content-explanation">
-                    <p>An innovative web platform dedicated to the rental of a wide range of items, involving individuals, stores, and businesses</p>
+                    <p>What is <span>Givit</span></p>
+                    <p>An innovative web platform dedicated to the rental of a wide range of items, involving
+                        individuals, stores, and businesses</p>
                     <div className="homepage-content-explanation-button">App</div>
-                    <div className="homepage-content-explanation-images">
-                        <div></div>
-                        <div></div>
-                    </div>
+                </div>
+                <div className="homepage-content-bottom-images">
+                    <img src={homepageExplanationImage1} alt=""></img>
+                    <img src={homepageExplanationImage2} alt=""></img>
+                    <img src={homepageExplanationImage3} alt=""></img>
+                    <img src={homepageExplanationImage4} alt=""></img>
+                    <img src={homepageExplanationImage5} alt=""></img>
+                    <img src={homepageExplanationImage6} alt=""></img>
                 </div>
                 <div className="homepage-content-sustainability">
                     <p>If your first choice is secondhand, this is <span>your place!</span></p>
-                    <p>When you engage in the seemingly straightforward acts of purchasing and renting through Givit,
-                        you're actually contributing to a multitude of positive outcomes. It involves breathing new life
-                        into items like that unused paddle racket, while also earning a bit of extra income. It includes
-                        the satisfaction of uncovering unexpected treasures and saving a few euros on items you desire.
-                        Moreover, by adopting a more conscious approach to consumption, you play a role in fostering a
-                        greener, more sustainable planet. Using Givit transforms you into a vital member of a vast
-                        community, numbering in the millions, who have embraced a superior way of procuring, sharing,
-                        and leading a more fulfilling life. So, what will be your first choice?</p>
+                    <p>When you use Givit for purchasing and renting, you're not just buying or renting items.
+                        You're revitalizing unused goods, earning extra money, discovering treasures, and saving money.
+                        By being mindful of your consumption, you contribute to a greener planet. Givit connects you
+                        with millions of others who embrace a better way of living. So, that will be your first
+                        choice?</p>
                     <div className="homepage-content-sustainability-button">Sustainability</div>
-                    <div className="homepage-content-sustainability-images">
-                        <div className="homepage-content-sustainability-images-row">
-                            <div></div>
-                            <div></div>
-                        </div>
-                        <div className="homepage-content-sustainability-images-row">
-                            <div></div>
-                        </div>
-                    </div>
                 </div>
             </div>
-            <LogoBottom />
+            <LogoBottom/>
             <Footer/>
         </>
     );
